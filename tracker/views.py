a10de6dcd0d4232a67bfa7711c8e4121c0f0348b
KhushbooItIs <workkhushboo16@gmail.com>
2026-03-21 16:37:27 +0530

    added model for expense tracking

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import Expenseform
 
-# Create your views here.
+def index(request):
+    if request.method=="POST":
+        expense=Expenseform(request.POST)
+        if expense.is_valid():
+            expense.save()
+      
+    expense_form=Expenseform()
+    return render( request,'tracker/index.html',{'expense_form':expense_form})
