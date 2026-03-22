727eacf33032ad6dfe89c9e6e0dd25b86a59854b
KhushbooItIs <workkhushboo16@gmail.com>
2026-03-23 05:19:34 +0530

    Expenses on basis of timeline

--- tracker/views.py
+++ tracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import Expenseform
 from .models import Expense
 from django.db.models import Sum
+import datetime
 
 def index(request):
     if request.method=="POST":
@@ -12,7 +13,32 @@ def index(request):
     total_expense=expenses.aggregate(Sum("amount"))['amount__sum']
     print(total_expense)
     expense_form=Expenseform()
-    return render( request,'tracker/index.html',{'expense_form':expense_form,'expenses':expenses,'total_expense':total_expense})
+    
+    
+    last_year=datetime.date.today()-datetime.timedelta(days=365)
+
+    data=Expense.objects.filter(date__gt=last_year)
+
+    Yearly_sum=expenses.aggregate(Sum("amount"))['amount__sum']
+
+
+    last_month=datetime.date.today()-datetime.timedelta(days=30)
+
+    data=Expense.objects.filter(date__gt=last_month)
+    monthly_sum=expenses.aggregate(Sum("amount"))['amount__sum']
+
+    last_week=datetime.date.today()-datetime.timedelta(days=7)
+
+    data=Expense.objects.filter(date__gt=last_week)
+    weekly_sum=expenses.aggregate(Sum("amount"))['amount__sum']
+
+
+
+    return render( request,'tracker/index.html',{'expense_form':expense_form,'expenses':expenses,
+                                                 'total_expense':total_expense,
+                                                 'yearly_sum': Yearly_sum,
+                                                 'monthly_sum':monthly_sum,
+                                                 'weekly_sum':weekly_sum})
 
 
 def edit(request,id):
