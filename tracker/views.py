85b3a5d0a059f67dd3633e3b912e1b6d0d21acc4
KhushbooItIs <workkhushboo16@gmail.com>
2026-03-23 02:39:16 +0530

    Table Creation

--- tracker/views.py
+++ tracker/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .forms import Expenseform
+from .models import Expense
 
 def index(request):
     if request.method=="POST":
         expense=Expenseform(request.POST)
         if expense.is_valid():
             expense.save()
-      
+    expenses=Expense.objects.all()
     expense_form=Expenseform()
-    return render( request,'tracker/index.html',{'expense_form':expense_form})
+    return render( request,'tracker/index.html',{'expense_form':expense_form,'expenses':expenses})
